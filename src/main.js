f8e50aec704fc61305a7b2ddfe80451927f4d4f1
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-01-18 17:57:06 +0100

    add social icons elements to Header info

--- src/main.js
+++ src/main.js
@@ -8,10 +8,22 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
+
+//brands
+import {faFacebookF} from '@fortawesome/free-brands-svg-icons'
+import {faPinterestP} from '@fortawesome/free-brands-svg-icons'
+import {faTwitter} from '@fortawesome/free-brands-svg-icons'
+
+//solid
 import { faPhone } from '@fortawesome/free-solid-svg-icons'
 
 /* add icons to the library */
-library.add(faPhone)
+library.add(
+    faFacebookF,
+    faPhone,
+    faPinterestP,
+    faTwitter
+    )
 
 createApp(App)
 .component('font-awesome-icon', FontAwesomeIcon)
