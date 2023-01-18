25e288abc2cf4f85f49c9b20d1e3b80d582650ed
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-01-18 17:58:48 +0100

    HeaderInfo component elements

--- src/components/AppHeaderComponents/HeaderInfo.vue
+++ src/components/AppHeaderComponents/HeaderInfo.vue
@@ -1,6 +1,16 @@
 <template>
     <div class="container">
-        
+        <div class="contacts">
+            <font-awesome-icon icon="fa-solid fa-phone" />
+            <span>(001)88451234</span>
+            <span>88455438</span>
+        </div>
+
+        <div class="socials">
+            <font-awesome-icon icon="fa-brands fa-facebook-f" />
+            <font-awesome-icon icon="fa-brands fa-twitter" />
+            <font-awesome-icon icon="fa-brands fa-pinterest-p" />
+        </div>
     </div>
 </template>
 
@@ -11,5 +21,28 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-    
+
+@use '../../styles/partials/colors.scss' as *;
+@use '../../styles/partials/alignment.scss' as *;
+    .container{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 40px;
+        padding: $h-p-standard;
+        background-color: $bg-dark-1;
+        color: $c-lightest;
+
+        .contacts{
+            width: 210px;
+            display: flex;
+            justify-content: space-between;
+        }
+
+        .socials{
+            width: 85px;
+            display: flex;
+            justify-content: space-between;
+        }
+    }
 </style>
