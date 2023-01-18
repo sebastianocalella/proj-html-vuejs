858084c06fa1cb0e8ec58f9fdb69f3db061d239c
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-01-18 16:57:35 +0100

    introduce App.vue components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default{
+  components: {
+            AppHeader,
+            AppMain,
+            AppFooter
+        },
+}
 
 </script>
 
 <template>
-
+  <AppHeader/>
+  <AppMain/>
+  <AppFooter/>
 </template>
 
 <style lang="scss">
