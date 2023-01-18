b271da8b6aad3b2515a2c3d2b23de203ea27cc50
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-01-18 20:41:24 +0100

    implement divider component

---
+++ src/components/StyleComponents/ComponentDivider.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="divider-container">
+        <div class="small"></div>
+        <div class="large"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ComponentDivider'
+}
+</script>
+
+<style lang="scss" scoped>
+@use '../../styles/partials/colors' as *;
+    .divider-container{
+        display: flex;
+        justify-content: space-between;
+        width: 64px;
+        height: 5px;
+        margin: 25px 0 36px;
+        
+        .small{
+            width: 9px;
+            background-image: linear-gradient(to right, $c-brand-green, $c-brand-blue);
+            border-radius: 2px;
+        }
+        .large{
+            width: 48px;
+            background: linear-gradient(to right, $c-brand-green, $c-brand-blue);
+            border-radius: 2px;
+        }
+    }
+</style>
