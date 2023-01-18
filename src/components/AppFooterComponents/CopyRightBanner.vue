cb089e9eb53725c6d64e171a0f24f57911f98ed0
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-01-18 20:57:11 +0100

    implement copyright banner

--- src/components/AppFooterComponents/CopyRightBanner.vue
+++ src/components/AppFooterComponents/CopyRightBanner.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        
+    <div class="container">
+        <span>&copy; 2020 phlox businnes theme</span>
     </div>
 </template>
 
@@ -11,5 +11,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-    
+@use '../../styles/partials/colors' as *;
+@use '../../styles/partials/alignment' as *;
+    .container{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 112px;
+        background: linear-gradient(to right, $bg-dark-1,$bg-darkest-t3);
+        padding: $h-p-standard;
+        color: $c-lightest-t-6;
+    }
 </style>
