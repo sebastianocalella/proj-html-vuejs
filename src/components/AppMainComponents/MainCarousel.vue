76963e6a1a4f9349134a21ff88334e18b9500381
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-01-18 23:17:23 +0100

    MainCarousel text content elements & style

--- src/components/AppMainComponents/MainCarousel.vue
+++ src/components/AppMainComponents/MainCarousel.vue
@@ -4,8 +4,8 @@
             <span>17 years of experience</span>
             <h1>Focus on Your <span>Business</span></h1>
             <ComponentDivider/>
-            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. Separated they live in Bookmarksgrove</p>
-            <button>read more.</button>
+            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. Separated they live in Bookmarksgrove.</p>
+            <button>read more</button>
             <span>
                 <a href="">facebook </a>
                 - 
@@ -37,18 +37,19 @@ export default {
         flex-direction: column;
         align-items: flex-start;
         width: 635px;
-        height: 680px;
         padding: 175px 0 80px;
 
         >span{
             font-size: 1.2rem;
             text-transform: uppercase;
             font-weight: bold;
+            color: $c-dark-3;
 
             &:first-child{
                 background: -webkit-linear-gradient($bg-gradient-green, $bg-gradient-yellow);
                 -webkit-background-clip: text;
                 -webkit-text-fill-color: transparent;
+                margin-bottom: 20px;
             }
         }
 
@@ -57,6 +58,34 @@ export default {
             span{
                 font-size: 6rem;
                 color: $c-brand-green;
+            }
+        }
+
+        p{
+            margin-bottom: 90px;
+            font-size: 1.1rem;
+            line-height: 1.9rem;
+        }
+
+        button{
+            margin-bottom: 120px;
+            height: 52px;
+            width: 160px;
+            border-radius: 80px;
+            color: $c-lightest;
+            text-transform: uppercase;
+            border: none;
+            background: linear-gradient(to right, $bg-dark-1, $bg-darkest-t3);
+            box-shadow: #605d6e77 0px 7px 29px 0px;
+            &:hover{
+                cursor: pointer;
+            }
+        }
+
+        a{
+            text-decoration: none;
+            &:visited{
+                color: $c-dark-3;
             }
         }
     }
