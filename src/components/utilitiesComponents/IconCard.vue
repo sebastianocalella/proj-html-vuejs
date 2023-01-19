ac936b9d4f3a387a63083c09a8fbe0ab176d127f
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-01-19 12:12:03 +0100

    Main Sections Subcomponents implement

---
+++ src/components/utilitiesComponents/IconCard.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <font-awesome-icon :icon="cardIcon" />
+        <h2>{{ cardTitle }}</h2>
+        <p>{{ cardText }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'IconCard',
+    props:{
+        cardIcon: String,
+        cardTitle: String,
+        cardText: String
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
---
+++ src/components/utilitiesComponents/ProductCard.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <h3>{{ title }}</h3>
+        <p>{{ subtitle }}</p>
+        <div>
+            <p>&dollar;<span>{{ price }}</span>{{ type }}</p>
+        </div>
+        <ul>
+            <li v-for="feature in features">
+            {{ feature }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProductCard',
+    props:{
+        title: String,
+        subtitle: String,
+        price: Number,
+        productType: String,
+        features: Array
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    @use '../../styles/partials/colors' as *;
+</style>
