50ecdc644cc83d3637d232b4ab5d222455be8cb3
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-01-20 10:39:38 +0100

    fixed position header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="container">
+    <header class="container">
         <HeaderInfo/>
         <HeaderNavbar/>
-    </div>
+    </header>
 </template>
 
 <script>
@@ -20,5 +20,12 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-    
+    header{
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        z-index: 3;
+        box-shadow: rgba(0, 0, 0, 0.2) 0px 5px 35px;
+    }
 </style>
